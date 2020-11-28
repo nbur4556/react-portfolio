50b4e7885f1a7a7375e3c4bdbc18fd4b73a64934
nbur4556 <nbur4556@gmail.com>
2020-11-28 12:40:52 -0600

    creates navigation content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 const Header = () => {
-    return (<div>Header</div>);
+    return (
+        <nav>
+            <h1>Nick Burt</h1>
+            <ul>
+                <li>Portfolio</li>
+                <li>Contact</li>
+                <li>Resume</li>
+            </ul>
+        </nav>);
 }
 
 export default Header;
