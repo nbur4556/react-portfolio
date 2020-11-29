d3e74dc001eba292432181a37ee513f3198ca525
nbur4556 <nbur4556@gmail.com>
2020-11-29 11:28:53 -0600

    creates and imports pages for about me, portfolio, and contact

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// Pages
+import AboutMe from './components/pages/AboutMe.js';
+import Portfolio from './components/pages/Portfolio.js';
+import Contact from './components/pages/Contact.js';
+
 // Components
 import Header from './components/Header.js';
 
