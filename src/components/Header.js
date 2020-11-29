4b5ae7d968d78ba7c750ca43f77385b04786eb92
nbur4556 <nbur4556@gmail.com>
2020-11-29 11:56:50 -0600

    adds navigation bar comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,14 @@ import resume from '../assets/web-dev-resume.pdf';
 const Header = () => {
     return (
         <nav>
+            {/* Main Link to Landing Page */}
             <Link className="pagelink" to="/"><h1>Nick Burt</h1></Link>
             <ul>
+                {/* Links to Website Pages */}
                 <Link className="pagelink" to="/portfolio"><li>Portfolio</li></Link>
                 <Link className="pagelink" to="/contact"><li>Contact</li></Link>
 
+                {/* Link to Resume Opens PDF in New Tab */}
                 <a className="pagelink" href={resume} target="_blank" rel="noreferrer"><li>Resume</li></a>
             </ul>
         </nav>
