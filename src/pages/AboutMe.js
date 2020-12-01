ee9d48dcdd2a32a39ab33331ceb36d27d3c45039
nbur4556 <nbur4556@gmail.com>
2020-12-01 12:43:15 -0600

    includes about me content

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const AboutMe = () => {
-    return (<div>About Me</div>);
+    return (
+        <section>
+            <h1>Hi, I'm Nick, and I am a Full Stack Web Developer.</h1>
+
+            <img href="placeholder.jpg" alt="My profile" />
+
+            <p>I am a full stack developer based in Austin Texas. I have a passion in creating beautiful and responsive web applications,
+                powered by scalable web servers and databases.</p>
+            <p>My skill range includes HTML + CSS + Javascript, React, NodeJS w/ Express, jQuerry, MySQL, and MongoDB.</p>
+            <p>I look forward to working with you!</p>
+        </section>
+    );
 }
 
 export default AboutMe;
