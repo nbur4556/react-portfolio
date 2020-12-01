8abff504d09c07003c4c811dedd3c27bf0fd1af0
nbur4556 <nbur4556@gmail.com>
2020-12-01 13:27:17 -0600

    includes profile picture

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+// Assets
+import profPic from '../assets/images/prof-pic.png';
+
 const AboutMe = () => {
     return (
         <section>
             <h1>Hi, I'm Nick, and I am a Full Stack Web Developer.</h1>
 
-            <img href="placeholder.jpg" alt="My profile" />
+            <img src={profPic} alt="My profile" />
 
             <p>I am a full stack developer based in Austin Texas. I have a passion in creating beautiful and responsive web applications,
                 powered by scalable web servers and databases.</p>
