25cb129e28768738abd07fb7624752a13cb6a1b7
nbur4556 <nbur4556@gmail.com>
2020-12-01 13:36:20 -0600

    sets initial grid layout for about me page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
 // Assets
+import '../stylesheets/about-me.css';
 import profPic from '../assets/images/prof-pic.png';
 
 const AboutMe = () => {
     return (
-        <section>
+        <section className="about-page">
             <h1>Hi, I'm Nick, and I am a Full Stack Web Developer.</h1>
 
             <img src={profPic} alt="My profile" />
 
-            <p>I am a full stack developer based in Austin Texas. I have a passion in creating beautiful and responsive web applications,
-                powered by scalable web servers and databases.</p>
-            <p>My skill range includes HTML + CSS + Javascript, React, NodeJS w/ Express, jQuerry, MySQL, and MongoDB.</p>
-            <p>I look forward to working with you!</p>
+            <div className="message">
+                <p>I am a full stack developer based in Austin Texas. I have a passion in creating beautiful and responsive web applications,
+                    powered by scalable web servers and databases.</p>
+                <p>My skill range includes HTML + CSS + Javascript, React, NodeJS w/ Express, jQuerry, MySQL, and MongoDB.</p>
+                <p>I look forward to working with you!</p>
+            </div>
         </section>
     );
 }
