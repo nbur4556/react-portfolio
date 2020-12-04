d90cde74a7bd8f7f34e5b7e7e9fd74fd92a99de1
nbur4556 <nbur4556@gmail.com>
2020-12-04 12:59:44 -0600

    adds title and description values to projects

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -12,30 +12,38 @@ import imgCodeRefactor from '../assets/images/project-main-code-refactor.jpg';
 
 const projects = [
     {
+        title: "Bucket Budget",
+        description: "description",
         image: imgBucketBudget,
         imageAlt: "Bucket Budget Homepage"
     },
     {
+        title: "Covid City Limits",
+        description: "description",
         image: imgCovidCity,
         imageAlt: "Covid City Limits Homepage"
     },
     {
+        title: "Weather Dashboard",
+        description: "description",
         image: imgWeatherDash,
         imageAlt: "Weather Dashboard Homepage"
     },
     {
+        title: "Workday Scheduler",
+        description: "description",
         image: imgWorkScheduler,
         imageAlt: "Workday Scheduler Homepage"
     },
     {
+        title: "Code Refactor",
+        description: "description",
         image: imgCodeRefactor,
         imageAlt: "Code Refactor Homepage"
     }
 ];
 
 const Portfolio = () => {
-
-    console.log(projects[0].image);
 
     return (
         <section className="portfolio-page">
@@ -44,6 +52,8 @@ const Portfolio = () => {
             {projects.map((project, index) => {
                 return <ProjectPreview
                     key={index}
+                    projectTitle={project.title}
+                    projectDescription={project.description}
                     imageSrc={project.image}
                     imageAltText={project.imageAlt}
                 />
