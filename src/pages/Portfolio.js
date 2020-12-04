ca95c4f3e3f4eccd51c3d02e799b8125b5301f9c
nbur4556 <nbur4556@gmail.com>
2020-12-04 13:06:56 -0600

    includes full descriptions

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -13,31 +13,31 @@ import imgCodeRefactor from '../assets/images/project-main-code-refactor.jpg';
 const projects = [
     {
         title: "Bucket Budget",
-        description: "description",
+        description: "Bucket Budget is a project built with a group of four, which allows a user to plan and budget for a trip or vacation. My role in the group was the head for backend development, so I was in charge of creating the server, and creating an api that worked with Triposo and our own back end database. The project had a need to remember user information in order to save and load their trips. For this, I was able to integrate Google's sign in API. This was chosen to remove the liability of saving passwords, while still giving our users all the benefits of having an account and saving information to our database.",
         image: imgBucketBudget,
         imageAlt: "Bucket Budget Homepage"
     },
     {
         title: "Covid City Limits",
-        description: "description",
+        description: "Covid City Limits was a project where a user could create their own dream music festival. As part of a group of five, we created a webpage that utilized both the Youtube and Bands in Town APIs so that users would be able to create their own online music festival. Features include creating a festival poster from a list of headliners and artists, creating a playlist of music videos, and viewing an artists current concerts and events.",
         image: imgCovidCity,
         imageAlt: "Covid City Limits Homepage"
     },
     {
         title: "Weather Dashboard",
-        description: "description",
+        description: "The Weather Dashboard uses Open Weather API to display data on current weather and five day forecast for a given location. The application takes advantage of local storage to save a history of searched cities, which allows for easily selecting a previous searched city.",
         image: imgWeatherDash,
         imageAlt: "Weather Dashboard Homepage"
     },
     {
         title: "Workday Scheduler",
-        description: "description",
+        description: "The workday scheduler application can be used to store tasks throughout the day. The UI updates in real time, and takes advantage of local storage to save the users schedule to their device. Moment JS is used to incorporate dates and times.",
         image: imgWorkScheduler,
         imageAlt: "Workday Scheduler Homepage"
     },
     {
         title: "Code Refactor",
-        description: "description",
+        description: "In the code refactor project, I took an existing web site and made it accessible. To achieve this, first I used semantic HTML to make sure it could be crawled and parsed by a variety of browsers. I made sure each image had an alt attribute, and finally I tweaked the colors to better support low vision and color blindness.",
         image: imgCodeRefactor,
         imageAlt: "Code Refactor Homepage"
     }
@@ -47,8 +47,6 @@ const Portfolio = () => {
 
     return (
         <section className="portfolio-page">
-            Portfolio
-
             {projects.map((project, index) => {
                 return <ProjectPreview
                     key={index}
