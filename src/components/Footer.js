339e8ef0efd6ce72d0935bbb889297229d85c732
nbur4556 <nbur4556@gmail.com>
2020-12-05 14:49:24 -0600

    finalizes styling for header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../stylesheets/navbars.css';
 
 const Footer = () => {
     return (
-        <section className="navbar">
+        <section className="navbar footer">
             <ul>
                 <a className="pagelink" href="https://github.com/nbur4556" target="_blank" rel="noreferrer"><li>Github</li></a>
                 <a className="pagelink" href="https://www.linkedin.com/in/nick-burt/" target="_blank" rel="noreferrer"><li>LinkedIn</li></a>
