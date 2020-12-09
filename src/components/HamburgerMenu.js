68ba787c2d174d7ad26972b3a974a9c56804963a
nbur4556 <nbur4556@gmail.com>
2020-12-09 16:12:52 -0600

    feat: create working links and correct sizing on dropdown box

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// Assets
 import '../stylesheets/hamburger-menu.css';
+import resume from '../assets/web-dev-resume.pdf';
 
 const HamburgerMenu = () => {
     const [dropdownState, setDropdownState] = useState({
@@ -19,8 +23,11 @@ const HamburgerMenu = () => {
             </div>
 
             <ul className={dropdownState.displayMenuClass}>
-                <li>Link1</li>
-                <li>Link2</li>
+                <Link className="pagelink" to="/portfolio"><li>Portfolio</li></Link>
+                <Link className="pagelink" to="/contact"><li>Contact</li></Link>
+
+                {/* Link to Resume Opens PDF in New Tab */}
+                <a className="pagelink" href={resume} target="_blank" rel="noreferrer"><li>Resume</li></a>
             </ul>
         </section>
     )
