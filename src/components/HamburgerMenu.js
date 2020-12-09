b1aa95833435d8f1a883052d4c8f4db257b1bb1c
nbur4556 <nbur4556@gmail.com>
2020-12-09 15:54:05 -0600

    feat: menu icon toggles hide and display dropdown menu list

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../stylesheets/hamburger-menu.css';
 
 const HamburgerMenu = () => {
+    const [dropdownState, setDropdownState] = useState({
+        displayMenuClass: "hide-dropdown"
+    });
+
+    const displayDropdownMenu = () => {
+        (dropdownState.displayMenuClass === "hide-dropdown") ? setDropdownState({ displayMenuClass: "show-dropdown" }) : setDropdownState({ displayMenuClass: "hide-dropdown" })
+    }
+
     return (
-        <section className="hamburger-menu">
+        <section className="hamburger-menu" onClick={displayDropdownMenu}>
             <div className="hamburger-icon">
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
 
-            <ul>
+            <ul className={dropdownState.displayMenuClass}>
                 <li>Link1</li>
                 <li>Link2</li>
             </ul>
